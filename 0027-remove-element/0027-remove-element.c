476e9a5c3470e0b0442ede904120b49d05f70013
DINH XUAN HIEU <dinhxuanhieuct@gmail.com>
2026-01-19 23:25:13 +0700

    Update 0027-remove-element.c

--- 0027-remove-element/0027-remove-element.c
+++ 0027-remove-element/0027-remove-element.c
@@ -2,7 +2,7 @@ int removeElement(int* nums, int numsSize, int val) {
     int i, k =0;
     for(i = 0; i< numsSize; i++){
         if(nums[i] != val){
-        	nums[k] = nums[i];
+        	nums[k] = nums[i]; #Just change the value of the index, judging just care about the element in range k returned 
         	k++;
     	}       
     }
